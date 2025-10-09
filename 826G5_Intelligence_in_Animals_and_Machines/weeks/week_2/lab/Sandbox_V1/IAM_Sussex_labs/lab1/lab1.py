4b227779dad3bf3c257c7e5547e9a1bb89658e80
luke_birkett <contactbirkett@gmail.com>
2025-10-09 10:59:41 +0100

    intell lab

---
+++ 826G5_Intelligence_in_Animals_and_Machines/weeks/week_2/lab/Sandbox_V1/IAM_Sussex_labs/lab1/lab1.py
@@ -0,0 +1,131 @@
+import sys
+# relative path to folder which contains the Sandbox module
+sys.path.insert(1, '../..')
+from Sandbox import *
+
+#########################################################################
+#                        Controller section                             #
+#########################################################################
+
+
+def aggressor(dt, inputs, params, state=[]) -> List[float]:
+
+    # set left motor speed
+    left_speed_command = math.sin(4) + params[0]
+    # set right motor speed
+    right_speed_command = left_speed_command * params[0]
+
+    # return motor speeds to robot's controller
+    return [left_speed_command, right_speed_command]
+
+aggressor_controller = BraitenbergController(step_fun=aggressor, gain=1)
+
+def coward(dt, inputs, params, state=[]) -> List[float]:
+
+    # set left motor speed
+    left_speed_command = math.sin(params[0]) - inputs[0]
+    # set right motor speed
+    right_speed_command = left_speed_command * params[0]
+
+    # return motor speeds to robot's controller
+    return [left_speed_command, right_speed_command]
+
+coward_controller = BraitenbergController(step_fun=coward, gain=0.1)
+
+def lover(dt, inputs, params, state=[]) -> List[float]:
+
+    # set left motor speed
+    left_speed_command = inputs[0] * math.cos(params[0])
+    # set right motor speed
+    right_speed_command = inputs[1] ** params[0] - inputs[0]
+
+    # return motor speeds to robot's controller
+    return [left_speed_command, right_speed_command]
+
+lover_controller = BraitenbergController(step_fun=lover, gain=2)
+
+def monocular(dt, inputs, params, state=[]) -> List[float]:
+
+    # set left motor speed
+    left_speed_command = params[0] - inputs[0]
+    # set right motor speed
+    right_speed_command = left_speed_command * inputs[1]
+
+    # return motor speeds to robot's controller
+    return [left_speed_command, right_speed_command]
+
+monocular_controller = BraitenbergController(step_fun=monocular, gain=0.1)
+
+#########################################################################
+#                           Noise section                               #
+#########################################################################
+
+# noise parameters for the robot's left motor
+max_white_noise = 0. # try 0.1 and -0.1 at first for white noise params
+min_white_noise = -0.
+max_brown_noise_step = 0.0 # try 0.01 at first
+spike_noise_prob = 0. # try 0.05 at first
+pos_spike_size = 1 # if the prob param is 0, there are no spikes, so these params have no effect
+neg_spike_size = -1
+
+# construct noise source for robot's left motor
+left_motor_noisemaker = NoiseMaker(white_noise_params=[max_white_noise, min_white_noise], brown_noise_step=max_brown_noise_step, spike_noise_params=[spike_noise_prob, pos_spike_size, neg_spike_size])
+
+#########################################################################
+#                      Simulation parameters                            #
+#########################################################################
+
+# select controller to use, from this list: [aggressor_controller, coward_controller, lover_controller, monocular_controller]
+controller = lover_controller
+# robot sensor parameters
+field_of_view = 0.9 * math.pi
+left_sensor_angle = np.pi/6
+right_sensor_angle = -np.pi/6
+# simulation run parameters
+screen_width = 700 # height and width of animation window, in pixels
+duration = 80 # number of simulation time units to simulate for
+n_runs = 10 # number of simulations to run
+animate = True # whether or not to animate
+animation_frame_delay = 0 # the 10s of milliseconds to pause between frames
+
+#########################################################################
+#                       Simulation section                              #
+#########################################################################
+
+# set up environment
+light_sources = [LightSource(0, 0)]
+# get robot
+robot = light_seeking_Robot(x=0, y=0, theta=0, light_sources=light_sources, controller=controller, left_motor_noisemaker=left_motor_noisemaker, FOV=field_of_view, left_sensor_angle=left_sensor_angle, right_sensor_angle=right_sensor_angle, left_motor_inertia=10, right_motor_inertia=10)
+# put robot into list of agents for Simulator
+agents=[robot]
+# In future labs, this list will not be empty - disturbances are going to be very important
+disturbances = []
+
+# get Simulator object - note that agents, environmental systems, and disturbances are all passed to its constructor
+sim = Simulator(agents=agents, envs=light_sources, duration=duration, dt=0.1, disturbances=disturbances)
+
+# use SimulationRunner to run Simulation n number of times
+sim_data = []
+for i in range(n_runs):
+
+    alpha = random_in_interval(minimum=-math.pi, maximum=math.pi)
+    rad = 7
+    n = random_in_interval(minimum=-math.pi/5, maximum=math.pi/5)
+    robot.push(x=rad*math.cos(alpha), y=rad*math.sin(alpha), theta=alpha+math.pi+n)
+
+    # get SimulationRunner object
+    sim_runner = SimulationRunner(sim, animate=animate, pause_ani=True, animation_delay=animation_frame_delay, screen_width=screen_width)
+    run_data = sim_runner.run_sims(n=1)
+    sim_data.append(run_data[0])
+    sim.reset()
+
+# plot agents' trajectories
+ax = plot_all_sims_trajectories(sim_data, show_cbar=False, cbar_fig=True)
+for light in light_sources:
+    light.draw(ax)
+# plot robots' basic data (plots of robot's various noisemakers are not produced by this function call)
+plot_all_robots_basic_data(sim_data, multiple_plots=False, show_motors=True, show_controllers=True, show_sensors=True)
+# plot noise
+plot_all_robots_noise(sim_data)
+
+plt.show()
---
+++ 826G5_Intelligence_in_Animals_and_Machines/weeks/week_2/lab/Sandbox_V1/Sandbox/base.py
@@ -0,0 +1,161 @@
+import numpy as np
+import math
+import time
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.transforms as mtransforms
+
+from typing import Dict, Any, Union, List, Callable, Tuple
+
+pygame = None
+try:
+    import pygame
+except:
+    print("WARNING: pygame module not found, visualisations will not be shown. " +
+          "You should be able to install pygame with:\n" +
+          "     pip install pygame")
+
+####################################################################################
+#                           utility functions begin
+####################################################################################
+
+# for any two angles, return difference in the interval of [-pi, pi]
+def angle_difference(angle1: float, angle2: float) -> float:
+    diff = (angle1 - angle2) % (2*math.pi)
+    if diff > math.pi:
+        diff -= (2*math.pi)
+    return diff
+
+# generate random number from uniform interval
+# - numpy already has a function for this, but I wrote this and used it in many places before thinking to check that
+def random_in_interval(minimum: float=0, maximum: float=1) -> float:
+    width = maximum - minimum
+    return (width * np.random.random()) + minimum
+
+####################################################################################
+#                           utility functions end
+####################################################################################
+####################################################################################
+#                           System class begins
+####################################################################################
+
+# Conceptually, all entities in the simulation are systems. For this reason,
+# all other classes inherit from this one.
+class System:
+    # construct System. Many systems have xy-coordinates and orientations (theta),
+    # but for some, such as Controllers and Disturbances, it is not normally useful
+    # to give them these variables.
+    # For those systems, has_position and/or has_orientation are set to False.
+
+    '''
+        Every object in a Sandox simulation is an instance of a subclass of the abstract class :class:`System`. In some cases, this is for conceptual reasons rather than practical ones, e.g. in the case of a :class:`DisturbanceSource`, which certainly can be considered a system but which doesn't currently inherit anything from :class:`System` (although this may well change in a future implementation).
+    '''
+    def __init__(self, x: float=None, y: float=None, theta: float=None):
+        """
+            __init__(x: float=None, y: float=None, theta: float=None)
+
+            Construct :class:`System`. If either ``x`` or ``y`` are specified in the call to ``init``, then the system will have position and will keep a history of both its x- and y-coordinates over time. If ``theta`` is specified in the call to ``__init__``, then the system has orientation, and will keep a history of its orientation over time.
+
+            :param x: The system's x-coordinate. Defaults to `None`.
+            :type x: float
+
+            :param y: The system's y-coordinate. Defaults to `None`.
+            :type y: float
+
+            :param theta: The system's angular orientation. Defaults to `None`. In *Sandbox*, orientations are in radians.
+            :type theta: float
+        """
+        self.has_position = (x is not None) or (y is not None)
+        if self.has_position:
+            self.init_xy(x, y)
+        self.has_orientation = theta is not None
+        if self.has_orientation:
+            self.init_theta(theta)
+
+    # systems with position and/or orientation will *need* to call this method,
+    # from their own step method
+    def step(self, dt: float) -> None:
+        '''
+            Step the :class:`System` forwards in time. Subclasses of :class:`System` will generally override this method, to implement class-specific functionality, but they will also need to call this method if they have either position or orientation, as this is where the history of thos variables over time gets updated.
+
+            :param dt: The interval of time to integrate the system over. Currently unused here, but will often be used in subclasses.
+            :type dt: float
+        '''
+        if self.has_position:
+            self.xs.append(self.x)
+            self.ys.append(self.y)
+        if self.has_orientation:
+            self.thetas.append(self.theta)
+
+    def get_data(self) -> Dict[str, Union[float, List[float]]]:
+        '''
+            A function to get the data from a :class:`System`, in the form of a string-keyed dict. If a :class:`System` has position, then its current coordinates plus their histories will be included in the data. If a :class:`System` has orientation, then its current orientation and its orientation history are incuded in the data.
+
+            These data, as and when they are included in the returned dict, can be accessed with the following keys:
+
+            * current x-coordinate: ``data["x"]``
+            * history of x-coordinates over time: ``data["xs"]``
+            * current y-coordinate: ``data["y"]``
+            * history of y-coordinates over time: ``data["ys"]``
+            * current orientation: ``data["theta"]``
+            * history of orientations over time: ``data["thetas"]``
+
+            :return: The System's data.
+            :rtype: dict
+        '''
+        data: Dict[str, Union[float, List[float]]] = {"x": None, "y": None,
+                                                      "theta": None, "xs": None,
+                                                      "ys": None, "thetas": None,
+                                                      "classname": "System"}
+        if self.has_position:
+            data["xs"] = self.xs
+            data["x"] = self.x
+            data["ys"] = self.ys
+            data["y"] = self.y
+        if self.has_orientation:
+            data["thetas"] = self.thetas
+            data["theta"] = self.theta
+
+        return data
+
+    def reset(self) -> None:
+        '''
+            Reset :class:`System` to its original state upon its construction, e.g. so that it can be re-used in another simulation run.
+        '''
+        if self.has_position:
+            self.init_xy(self.xs[0], self.ys[0])
+        if self.has_orientation:
+            self.init_theta(self.thetas[0])
+
+    def get_data_and_reset(self) -> Dict[str, dict]:
+        '''
+            Reset :class:`System` to its original state and return its data.
+        '''
+        data = self.get_data()
+        self.reset()
+        return data
+
+    def init_xy(self, x: float, y: float) -> None:
+        '''
+            Set the systems initial x- and y-coordinates to the passed in values.
+
+            :param x: The system's x-coordinate.
+            :type x: float
+
+            :param y: The system's y-coordinate.
+            :type y: float
+        '''
+        self.x = x
+        self.y = y
+        self.xs = [x]
+        self.ys = [y]
+
+    def init_theta(self, theta: float) -> None:
+        '''
+            Set the systems initial orientation to the passed in value.
+
+            :param theta: The system's orientation.
+            :type theta: float
+        '''
+        self.theta = theta
+        self.thetas = [theta]
